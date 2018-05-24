e14f47141674a001829515442c1793e0f298db9f
Reem Naqvi <reemnaqvi@gmail.com>
2018-05-24 14:20:39 -0500

    Update ShipColumbus.java

--- ShipColumbus.java
+++ ShipColumbus.java
@@ -5,6 +5,49 @@ import java.util.List;
 
 public class ShipColumbus implements Ship {
 	List<Observer> observers = new LinkedList<Observer>();
+	OceanMap OceanMap;
+	Point ShipLocation;
+
+	public ShipColumbus(OceanMap oceamMap) {
+		this.OceanMap = oceamMap;
+
+	}
+
+	public Point getShipLocation() {
+		return ShipLocation;
+	}
+	
+	
+		public void goEast() {
+		if (currentLocation.x < oceanMap.getDimensions() - 1
+				&& oceanMap.isOcean(currentLocation.x + 1, currentLocation.y)) {
+			currentLocation.x++;
+		}
+		notifyObservers();
+
+	}
+
+	public void goWest() {
+		if (currentLocation.x > 0 && oceanMap.isOcean(currentLocation.x - 1, currentLocation.y)) {
+			currentLocation.x--;
+		}
+		notifyObservers();
+	}
+
+	public void goNorth() {
+		if (currentLocation.y > 0 && oceanMap.isOcean(currentLocation.x, currentLocation.y - 1)) {
+			currentLocation.y--;
+		}
+		notifyObservers();
+	}
+
+	public void goSouth() {
+		if (currentLocation.y < oceanMap.getDimensions() - 1
+				&& oceanMap.isOcean(currentLocation.x, currentLocation.y + 1)) {
+			currentLocation.y++;
+		}
+		notifyObservers();
+
 
 	@Override
 	public void registerObserver(Observer o) {
