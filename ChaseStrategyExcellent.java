e1bf2faf70852acd0910a58aedfce43c23742eec
fldees <fldeees@gmail.com>
2018-05-29 17:45:47 -0500

    Update ChaseStrategyExcellent.java

--- ChaseStrategyExcellent.java
+++ ChaseStrategyExcellent.java
@@ -4,8 +4,12 @@ public class ChaseStrategyExcellent implements ChaseStrategy {
     
     Pirate pirate;
 
-    public void chase(Pirate pirate) {
+    public ChaseStrategyExcellent(Pirate pirate) {
         this.pirate = pirate;
+    }
+
+    public void chase(Pirate pirate) {
+        // chase method to be implemented
     }
 
 }
