fa82d412b3f519da7773c1cb8eed0c74ed95c174
fldees <fldeees@gmail.com>
2018-05-29 17:44:52 -0500

    Update ChaseStrategyStupid.java

--- ChaseStrategyStupid.java
+++ ChaseStrategyStupid.java
@@ -4,9 +4,13 @@ public class ChaseStrategyStupid implements ChaseStrategy {
     
     Pirate pirate;
     
-    public void chase(Pirate pirate) {
+    public ChaseStrategyStupid(Pirate pirate) {
         this.pirate = pirate;
+    }
+    
+    public void chase(Pirate pirate) {
         //chase method to be implemented...
     }
 
 }
+
