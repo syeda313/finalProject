9a4148607218d4ba6311cd9702fc0fb46d2e778f
Reem Naqvi <reemnaqvi@gmail.com>
2018-05-30 15:30:00 -0500

    Update ChaseStrategyStupid.java

--- ChaseStrategyStupid.java
+++ ChaseStrategyStupid.java
@@ -6,42 +6,38 @@ public class ChaseStrategyStupid implements ChaseStrategy {
     
     Pirate pirate;
     OceanMap oceanMap;
+    Random rand;
     
     
     
     public ChaseStrategyStupid(Pirate pirate) {
         this.pirate = pirate;
         oceanMap = pirate.getOceanMap();
+	rand = new Random();
     }
     
     @Override
     public void chase() {
         if (rand.nextInt(2) == 1) {
-			if (big.getPirateLocation().x - position.x < 0
-					&& oceanMap.getMap()[big.getPirateLocation().x + 1][big.getPirateLocation().y] == 0) {
-				big.getPirateLocation().x++;
-			} else if (oceanMap.getMap()[big.getPirateLocation().x - 1][big.getPirateLocation().y] == 0) {
-				big.getPirateLocation().x--;
+		if (rand.nextInt(2) == 1) {
+			if (pirate.getPirateLocation().x - oceanMap.getShipLocation().x < 0
+					&& oceanMap.getMap()[pirate.getPirateLocation().x + 1][pirate.getPirateLocation().y] == 0) {
+				pirate.getPirateLocation().x++;
+				oceanMap.updatePirateLocation(pirate.getPirateLocation(), pirate.getPirateID());
+			} else if (oceanMap.getMap()[pirate.getPirateLocation().x - 1][pirate.getPirateLocation().y] == 0) {
+				pirate.getPirateLocation().x--;
+				oceanMap.updatePirateLocation(pirate.getPirateLocation(), pirate.getPirateID());
 			}
-			if (big.getPirateLocation().y - position.y < 0
-					&& oceanMap.getMap()[big.getPirateLocation().x][big.getPirateLocation().y + 1] == 0) {
-				big.getPirateLocation().y++;
-			} else if (oceanMap.getMap()[big.getPirateLocation().x][big.getPirateLocation().y - 1] == 0) {
-				big.getPirateLocation().y--;
-			}
-			if (small.getPirateLocation().x - position.x < 0
-					&& oceanMap.getMap()[small.getPirateLocation().x + 1][small.getPirateLocation().y] == 0) {
-				small.getPirateLocation().x++;
-			} else if (oceanMap.getMap()[small.getPirateLocation().x - 1][small.getPirateLocation().y] == 0) {
-				small.getPirateLocation().x--;
-			}
-			if (small.getPirateLocation().y - position.y < 0
-					&& oceanMap.getMap()[small.getPirateLocation().x][small.getPirateLocation().y + 1] == 0) {
-				small.getPirateLocation().y++;
-			} else if (oceanMap.getMap()[small.getPirateLocation().x][small.getPirateLocation().y - 1] == 0) {
-				small.getPirateLocation().y--;
+			if (pirate.getPirateLocation().y - oceanMap.getShipLocation().y < 0
+					&& oceanMap.getMap()[pirate.getPirateLocation().x][pirate.getPirateLocation().y + 1] == 0) {
+				pirate.getPirateLocation().y++;
+				oceanMap.updatePirateLocation(pirate.getPirateLocation(), pirate.getPirateID());
+			} else if (oceanMap.getMap()[pirate.getPirateLocation().x][pirate.getPirateLocation().y - 1] == 0) {
+				pirate.getPirateLocation().y--;
+				oceanMap.updatePirateLocation(pirate.getPirateLocation(), pirate.getPirateID());
 			}
 		}
+			
         
         
     }
