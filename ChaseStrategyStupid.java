4525f8deb5d397938bfb755e37403b1543d0d6ed
Reem Naqvi <reemnaqvi@gmail.com>
2018-05-29 19:45:28 -0500

    Update ChaseStrategyStupid.java

--- ChaseStrategyStupid.java
+++ ChaseStrategyStupid.java
@@ -16,6 +16,32 @@ public class ChaseStrategyStupid implements ChaseStrategy {
     
     @Override
     public void chase() {
+        if (rand.nextInt(2) == 1) {
+			if (big.getPirateLocation().x - position.x < 0
+					&& oceanMap.getMap()[big.getPirateLocation().x + 1][big.getPirateLocation().y] == 0) {
+				big.getPirateLocation().x++;
+			} else if (oceanMap.getMap()[big.getPirateLocation().x - 1][big.getPirateLocation().y] == 0) {
+				big.getPirateLocation().x--;
+			}
+			if (big.getPirateLocation().y - position.y < 0
+					&& oceanMap.getMap()[big.getPirateLocation().x][big.getPirateLocation().y + 1] == 0) {
+				big.getPirateLocation().y++;
+			} else if (oceanMap.getMap()[big.getPirateLocation().x][big.getPirateLocation().y - 1] == 0) {
+				big.getPirateLocation().y--;
+			}
+			if (small.getPirateLocation().x - position.x < 0
+					&& oceanMap.getMap()[small.getPirateLocation().x + 1][small.getPirateLocation().y] == 0) {
+				small.getPirateLocation().x++;
+			} else if (oceanMap.getMap()[small.getPirateLocation().x - 1][small.getPirateLocation().y] == 0) {
+				small.getPirateLocation().x--;
+			}
+			if (small.getPirateLocation().y - position.y < 0
+					&& oceanMap.getMap()[small.getPirateLocation().x][small.getPirateLocation().y + 1] == 0) {
+				small.getPirateLocation().y++;
+			} else if (oceanMap.getMap()[small.getPirateLocation().x][small.getPirateLocation().y - 1] == 0) {
+				small.getPirateLocation().y--;
+			}
+		}
         
         
     }
