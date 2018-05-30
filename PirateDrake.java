44aa1fba958c71d82b19e3d5dbc7810251db4ad2
Reem Naqvi <reemnaqvi@gmail.com>
2018-05-30 15:34:17 -0500

    Update PirateDrake.java

--- PirateDrake.java
+++ PirateDrake.java
@@ -6,50 +6,56 @@ import java.util.Random;
 public class PirateDrake implements Pirate {
 
     OceanMap oceanMap;
-    Point pirateLocation;
-    ChaseStrategy chaseStrategy;
-    int dimensions;
-    Random rand = new Random();
-    
-    public PirateDrake(OceanMap oceanMap) {
-        this.oceanMap = oceanMap;
-        pirateLocation = placePirate();
-        chaseStrategy = new ChaseStrategyExcellent(this);
-        dimensions = oceanMap.getDimensions();
-    }
-    
-    public Point getPirateLocation() {
-        return pirateLocation;
-    }
-    
-    private Point placePirate() {
-        boolean placedShip = false;
-        int x=0,y=0;
-        while(!placedShip){
-            x = rand.nextInt(dimensions);
-            y = rand.nextInt(dimensions);
-            if(oceanMap.getMap()[x][y] == 0){
-                placedShip = true;
-            }
-        }
-        return new Point(x,y);
-    }
-    
-    public void changeStrategy(ChaseStrategy chaseStrategy) {
-        this.chaseStrategy = chaseStrategy;
-        this.chaseStrategy.setPirate(this);
+	Point pirateLocation;
+	ChaseStrategy chaseStrategy;
+	int pirateID;
+	int dimensions;
+	Random rand = new Random();
+
+	public PirateDrake(OceanMap oceanMap, int pirateID) {
+		this.oceanMap = oceanMap;
+		this.pirateID = pirateID;
+		dimensions = oceanMap.getDimensions(); // set the attributes first, before it is used
+		pirateLocation = placePirate();
+		chaseStrategy = new ChaseStrategyGood(this);
+	}
+
+	public Point getPirateLocation() {
+		return pirateLocation;
+	}
+	
+	public int getPirateID() {
+        return pirateID;
     }
 
-    @Override
-    public void update() {
-        chaseStrategy.chase();
-        // TODO Auto-generated method stub
-        
-    }
+	private Point placePirate() {
+		boolean placedShip = false;
+		int x = 0, y = 0;
+		while (!placedShip) {
+			x = rand.nextInt(dimensions);
+			y = rand.nextInt(dimensions);
+			if (oceanMap.getMap()[x][y] == 0) {
+				placedShip = true;
+			}
+		}
+		return new Point(x, y);
+	}
 
-    @Override
-    public OceanMap getOceanMap() {
-        return oceanMap;
-    }
+	public void changeStrategy(ChaseStrategy ChaseStrategy) {
+		this.chaseStrategy = ChaseStrategy;
+		this.chaseStrategy.setPirate(this);
+	}
+
+	@Override
+	public void update() {
+		chaseStrategy.chase();
+
+	}
+
+	@Override
+	public OceanMap getOceanMap() {
+		return oceanMap;
+	}
+
 
 }
