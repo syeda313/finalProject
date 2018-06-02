cc9ad54c3e69dd17164d5f610839ce9b10a56272
Reem <reemnaqvi@gmail.com>
2018-06-01 19:14:36 -0500

    Update OceanExplorer.java

--- OceanExplorer.java
+++ OceanExplorer.java
@@ -13,35 +13,35 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class OceanExplorer extends Application {
+	OceanMap oceanMap;
 	int[][] islandMap;
 	Pane root;
 	int dimensions;
 	final int scalingFactor = 20;
-	// final int islandCount = 10;
-	OceanMap oceanMap;
 	Ship ship;
 	Scene scene;
-	// Thread monstersThread;
-
-	// ArrayList<PirateShip> p = new ArrayList<PirateShip>();
 
 	Image shipImage;
 	ImageView shipImageView;
 
+	int numberofIslands = 10;
+	Image[] Island;
+	ImageView[] Islands;
+
+	Image[] island;
+	ImageView[] I;
+
 	Image Pship;
 	ImageView pirateShipView;
 
-	Image Island;
-	ImageView Islands;
-
 	int numberOfPirate = 10;
+	// int numofIslands = 10;
 	Pirate[] pirate;
 	Image[] pirateImage;
 	ImageView[] pirateImageView;
-	
+
 	ScaryMonster ScaryMonster;
 	Thread mons;
-
 
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -61,7 +61,7 @@ public class OceanExplorer extends Application {
 		}
 
 		loadShipImage();
-		
+
 		ScaryMonster = new ScaryMonster(scalingFactor);
 		ScaryMonster.addToPane(root.getChildren());
 
@@ -73,8 +73,9 @@ public class OceanExplorer extends Application {
 		stage.setScene(scene);
 		stage.show();
 		startSailing();
+
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@Override
 	public void stop() {
@@ -99,25 +100,40 @@ public class OceanExplorer extends Application {
 			pirateImageView[i].setY(pirate[i].getPirateLocation().y * scalingFactor);
 			root.getChildren().add(pirateImageView[i]);
 		}
-
 	}
 
 	public void drawMap() {
+
 		for (int x = 0; x < dimensions; x++) {
 			for (int y = 0; y < dimensions; y++) {
-
+				Rectangle rect = new Rectangle(x * scalingFactor, y * scalingFactor, scalingFactor, scalingFactor);
+				rect.setStroke(Color.BLACK);
 				if (islandMap[x][y] == 0) {
-					Rectangle rect = new Rectangle(x * scalingFactor, y * scalingFactor, scalingFactor, scalingFactor);
-					rect.setStroke(Color.BLACK);
+					System.out.println("///////////");
 					rect.setFill(Color.PALETURQUOISE);
 					root.getChildren().add(rect);
+
+				} //else if (islandMap[x][y] == 1) {
+					//System.out.println("1111111111111");
+					// Islands = new ImageView(Island);
+					// Islands.setX(x * scalingFactor);
+					// Islands.setY(y * scalingFactor);
+					// root.getChildren().add(Islands);
+
+				//} //else if (islandMap[x][y] == 2) {
+					//System.out.println("+++++++++++");
+					// I = new ImageView(Island);
+					// I.setX(x * scalingFactor);
+					// I.setY(y * scalingFactor);
+					// root.getChildren().add(I);
 				} else // Need to load an image
 				{
-					Rectangle rect = new Rectangle(x * scalingFactor, y * scalingFactor, scalingFactor, scalingFactor);
-					rect.setStroke(Color.BLACK);
-					rect.setFill(Color.RED);
-					root.getChildren().add(rect);
+					Rectangle rect1 = new Rectangle(x * scalingFactor, y * scalingFactor, scalingFactor, scalingFactor);
+					rect1.setStroke(Color.BLACK);
+					rect1.setFill(Color.RED);
+					root.getChildren().add(rect1);
 				}
+
 			}
 		}
 	}
@@ -158,7 +174,5 @@ public class OceanExplorer extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-
-	
 
 }
